5349e780289586f3d42bb98b2206108466302bf0
phanngocha <havica.jpr@gmail.com>
2024-01-31 07:52:24 +0700

    Slide 55 chuong 2

---
+++ BaiThucHanh/src/slide55chuong2.java
@@ -0,0 +1,10 @@
+/*Viet chuong trinh hien thi cac so chia het cho 5 trong pham vi tu 0 den 20 */
+public class slide55chuong2 {
+      public static void main(String[] args) {
+        System.out.print("Cac so chia het cho 5 la : ");
+        for(int i = 0; i<=20;i++)
+           if(i % 5 == 0){
+            System.out.print(i+" ");
+      }
+    }
+}
