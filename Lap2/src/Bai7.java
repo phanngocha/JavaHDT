469728031bf7421b1c49f51dca45a1fb61269aba
phanngocha <havica.jpr@gmail.com>
2024-02-04 08:54:55 +0700

    Bai 7

---
+++ Lap2/src/Bai7.java
@@ -0,0 +1,9 @@
+/*Viet chuong trinh in ra 20 so nguyen duong dau tien ra man hinh */
+public class Bai7{
+    public static void main(String[] args) {
+         for(int i=1;i<=20;i++)
+        {
+            System.out.print(+i+ "\t");
+        }
+    }
+}
