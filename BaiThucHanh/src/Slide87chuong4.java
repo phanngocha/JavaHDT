f8b5765100f45e732f14cbc44538c0e4c14c83de
phanngocha <havica.jpr@gmail.com>
2024-05-17 10:28:14 +0700

    Map

---
+++ BaiThucHanh/src/Slide87chuong4.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map;
+public class Slide87chuong4 {
+      public static void main(String[] args) {
+        HashMap<String,String> hashMap = new HashMap<>();
+
+        hashMap.put("CSLT", "Co so lap trinh");
+        hashMap.put("C++","C++");
+        hashMap.put("C#", "C Sharp");
+        hashMap.put("PHP", "PHP");
+        hashMap.put("Java", "Java");
+
+        Set<Map.Entry<String, String>> setHashMap = hashMap.entrySet();
+
+        System.out.println("cac entry co trong setHashMap: ");
+        System.out.println(setHashMap);
+      }   
+}
---
+++ BaiThucHanh/src/Slide89chuong4.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map;
+public class Slide89chuong4 {
+   public static void main(String[] args) {
+      HashMap<String, String> hashMapCity = new HashMap<>();
+      hashMapCity.put("QNg", "Quang Ngai");
+      hashMapCity.put("QN", "Quang Nam");
+      hashMapCity.put("QN", "Quang Ninh");
+      hashMapCity.put("HCM", "Thanh pho Ho Chi Minh");
+
+      System.out.println("Danh sach cac thanh pho trong hashMapCity: ");
+      Set<Map.Entry<String, String>> setCity = hashMapCity.entrySet();
+      System.out.println(setCity);
+      
+      System.out.println("QNg: "+hashMapCity.get("QNg"));
+      System.out.println("PN: "+hashMapCity.get("PN"));
+
+      if(hashMapCity.containsValue("Thanh pho Ho Chi Minh")){
+        System.out.println("Co thanh pho Ho Chi Minh trong hashMapCity");
+      }
+   }
+}
